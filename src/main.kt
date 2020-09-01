0d583e8a89668700f9ba208ed21f9f6f7285b89b
emkat93 <eallen5064@students.mchenry.edu>
2020-08-31 22:42:38 -0500

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,24 @@
+fun main(){
+    //numbers
+    var myByte: Byte = 5
+    var myShort: Short = 8
+    var myInt: Int = 12
+    var myLong: Long = 1078
+    var myFloat: Float = 9.64F
+    var myDouble: Double = 66.667
+
+    //boolean
+    var myBoolean: Boolean = false
+
+    //cha
+    var myChar = "A"
+    println("$myByte, Is a Byte. A byte is an 8-bit integer.")
+    println("$myShort, Is a Short which is an 16-bit integer.")
+    println("$myInt, Is an Int. An Int is a 32-bit integer.")
+    println("$myLong, Is a Long which means it's a 64-bit integer.")
+    println("$myFloat, Is a Float. A Float is a small number with a decimal point.")
+    println("$myDouble, Is a Double. A Double is a large number with a decimal point")
+    println("$myBoolean, Is a Boolean which can tell you is a statement is true or false.")
+    println("$myChar, Is a Char which is a unicode character.")
+
+}
